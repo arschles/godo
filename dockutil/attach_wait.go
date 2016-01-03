cb4135afc86c29afb04063212d545d94b94246a3
Aaron Schlesinger <arschles@gmail.com>
2016-01-02 22:00:03 -0800

    more docs

--- dockutil/attach_wait.go
+++ dockutil/attach_wait.go
@@ -4,6 +4,8 @@ import (
 	docker "github.com/fsouza/go-dockerclient"
 )
 
+// AttachAndWait attaches to, and waits for, the container with the given ID using the given client, according to the given options.
+// The first returned channel will receive if there was an error attaching. The second channel returned will receive if there was an error waiting. The 3rd channel will return with the exit code.
 func AttachAndWait(dockerClient *docker.Client, containerID string, attachOpts docker.AttachToContainerOptions) (<-chan error, <-chan error, <-chan int) {
 	attachErrCh := make(chan error)
 	go func() {
