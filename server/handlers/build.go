902d13dbd219cf850d799bebe08d717ecdee942e
Aaron Schlesinger <aschlesinger@deis.com>
2016-02-12 20:49:19 -0800

    implementing more of the build server

--- server/handlers/build.go
+++ server/handlers/build.go
@@ -2,12 +2,16 @@ package handlers
 
 import (
 	"archive/tar"
+	"bytes"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 
+	"github.com/arschles/gci/config"
 	"github.com/arschles/gci/log"
 )
 
@@ -26,6 +30,8 @@ func (b Build) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer os.Remove(tmpDir)
+	gciFileName := ""
+	var gciFileBytes bytes.Buffer
 	for {
 		hdr, err := tr.Next()
 		if err == io.EOF {
@@ -34,15 +40,38 @@ func (b Build) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			log.Err("Reading file (%s)", err)
 			break
 		}
+
 		fileName := filepath.Join(tmpDir, hdr.Name)
 		fd, err := os.Create(fileName)
 		if err != nil {
 			log.Err("Creating %s (%s)", fileName, err)
 			continue
 		}
-		if _, err := io.Copy(fd, tr); err != nil {
+		defer fd.Close()
+
+		var dest io.Writer = fd
+		if hdr.Name == config.DefaultFileNameYaml || hdr.Name == config.DefaultFileNameYml && gciFileName != "" {
+			dest = io.MultiWriter(fd, &gciFileBytes)
+			gciFileName = hdr.Name
+		} else {
+			str := fmt.Sprintf("multiple GCI config files found (%s and %s)", hdr.Name, gciFileName)
+			http.Error(w, str, http.StatusBadRequest)
+			return
+		}
+
+		if _, err := io.Copy(dest, tr); err != nil {
 			log.Err("Writing archived file to %s (%s)", fileName, err)
 			continue
 		}
 	}
+	cfg := config.Empty()
+	if gciFileName != "" {
+		c, err := config.ReadBytes(gciFileBytes.Bytes())
+		if err != nil {
+			http.Error(w, fmt.Sprintf("%s was invalid (%s)", gciFileName, err), http.StatusBadRequest)
+			return
+		}
+		cfg = c
+	}
+	io.Copy(w, strings.NewReader(fmt.Sprintf("%s", *cfg)))
 }
