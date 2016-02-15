9dd843b4972aa711c124f666eb535281fb5d18d8
Aaron Schlesinger <arschles@gmail.com>
2016-02-15 15:19:36 -0800

    add & use gorilla mux

--- actions/server/run.go
+++ actions/server/run.go
@@ -11,13 +11,15 @@ import (
 	dockutil "github.com/arschles/gci/util/docker"
 	fileutil "github.com/arschles/gci/util/file"
 	"github.com/codegangsta/cli"
+	"github.com/gorilla/mux"
 )
 
 func Run(c *cli.Context) {
 	dockerCl := dockutil.ClientOrDie()
 	mux := http.NewServeMux()
 	cfg := config.ReadOrDie(c.String(actions.FlagConfigFile))
-	mux.Handle("/build", handlers.NewBuild(dockerCl, fileutil.LocalTmpDirCreator()))
+	mux.Handle("/build", handlers.NewBuild(dockerCl, fileutil.LocalTmpDirCreator())).Methods("POST")
+	mux.Handle("/test", handlers.NewTest(dockerCl, fileutil.LocalTmpDirCreator())).Methods("POST")
 	hostStr := fmt.Sprintf("%s:%d", cfg.CI.Server.GetBindHost(), cfg.CI.Server.GetPort())
 	log.Info("Serving GCI on %s", hostStr)
 	log.Die(http.ListenAndServe(hostStr, mux).Error())
