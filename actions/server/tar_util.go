3339c73706d7be1af88bc8f6c22a32e0fad0cbd2
Aaron Schlesinger <arschles@gmail.com>
2016-02-15 08:51:12 -0800

    fixing too many open files on gci server build

--- actions/server/tar_util.go
+++ actions/server/tar_util.go
@@ -16,25 +16,30 @@ func (e errCreatingHeader) Error() string {
 	return fmt.Sprintf("error creating header for %s (%s)", e.path, e.err)
 }
 
+func writeFileToTarWriter(file string, tarWriter *tar.Writer) error {
+	fd, fInfo, err := openAndStatFile(file)
+	if err != nil {
+		return err
+	}
+	defer fd.Close()
+	hdr := &tar.Header{Name: file, Mode: int64(fInfo.Mode()), Size: fInfo.Size()}
+	if err := tarWriter.WriteHeader(hdr); err != nil {
+		return errCreatingHeader{path: file, err: err}
+	}
+	fileBytes, err := ioutil.ReadAll(fd)
+	if err != nil {
+		return err
+	}
+	if _, err := tarWriter.Write(fileBytes); err != nil {
+		return err
+	}
+	return nil
+}
+
 func createTarArchive(wr io.Writer, files []string) (*tar.Writer, error) {
 	tarWriter := tar.NewWriter(wr)
 	for _, file := range files {
-		fd, fInfo, err := openAndStatFile(file)
-		if err != nil {
-			return nil, err
-		}
-		hdr := &tar.Header{Name: file, Mode: int64(fInfo.Mode()), Size: fInfo.Size()}
-		if err := tarWriter.WriteHeader(hdr); err != nil {
-			return nil, errCreatingHeader{path: file, err: err}
-		}
-
-		fileBytes, err := ioutil.ReadAll(fd)
-		if err != nil {
-			return nil, err
-		}
-		if _, err := tarWriter.Write(fileBytes); err != nil {
-			return nil, err
-		}
+		writeFileToTarWriter(file, tarWriter)
 	}
 	if err := tarWriter.Close(); err != nil {
 		return nil, err
