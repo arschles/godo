3f661cbf6b02c2d00f80e9e2a19203bfb8de062c
Aaron Schlesinger <arschles@gmail.com>
2016-04-27 19:22:51 -0700

    removing all the server code

--- server/common/constants.go
+++
@@ -1,9 +0,0 @@
-package common
-
-const (
-	CrossCompileHeader = "X-GCI-Cross-Compile"
-	CrossCompileTrue   = "true"
-	CrossCompileFalse  = "false"
-	EnvHeader          = "X-GCI-EnvVar"
-	PackageNameHeader  = "X-GCI-Package-Name"
-)
--- server/handlers/config_file_util.go
+++
@@ -1,9 +0,0 @@
-package handlers
-
-import (
-	"github.com/arschles/gci/config"
-)
-
-func isGCIFileName(name string) bool {
-	return name == config.DefaultFileNameYaml || name == config.DefaultFileNameYml
-}
